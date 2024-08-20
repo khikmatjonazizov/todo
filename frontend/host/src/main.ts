e363bbb463f36e7028153a0ac1295402b0f91e3d
khikmatjon <azizovxikmatjon04@gmail.com>
2024-08-20 23:41:16 +0500

    working version of vue

--- frontend/host/src/main.ts
+++ frontend/host/src/main.ts
@@ -8,16 +8,11 @@ import { App as NavBarApp } from 'navbar/app'
 // @ts-ignore
 import { App as CreateTaskApp } from 'create_task/app'
 // @ts-ignore
-import { default as MainListApp } from 'main_list/app'
+import MainListApp from 'main_list/app'
 
 
 import { tasksApi } from 'api';
 import { MainListProps } from 'contract/main-list';
-
-const getTasks = async () => {
-  const tasks = await tasksApi.getAll();
-  return tasks;
-}
 
 const handleEditTask: MainListProps['handleEditTask'] = async (body) => {
   const task = await tasksApi.edit(body);
@@ -29,24 +24,18 @@ const handleDeleteTask: MainListProps['handleDeleteTask'] = async (id) => {
 }
 
 
-async function loadComponents() {
-  const tasks = await getTasks();
-
-  const mainListContainer = document.getElementById('main-list')
-  if (mainListContainer) {
-    const app = Vue.createApp(MainListApp, {tasks, handleEditTask, handleDeleteTask})
-    app.mount(mainListContainer)
-  }
-
-  const navbarContainer = document.getElementById('navbar')
-  if (navbarContainer) {
-    ReactDOM.createRoot(navbarContainer!).render(createElement(NavBarApp))
-  }
-
-  const createTaskContainer = document.getElementById('create-task')
-  if(createTaskContainer) {
-    ReactDOM.createRoot(createTaskContainer!).render(createElement(CreateTaskApp))
-  }
+const mainListContainer = document.getElementById('main-list')
+if (mainListContainer) {
+  const app = Vue.createApp(MainListApp, {tasks: [{id: '1', desc: 'test', done: true}], handleEditTask, handleDeleteTask})
+  app.mount(mainListContainer)
 }
 
-loadComponents()
+const navbarContainer = document.getElementById('navbar')
+if (navbarContainer) {
+  ReactDOM.createRoot(navbarContainer!).render(createElement(NavBarApp))
+}
+
+const createTaskContainer = document.getElementById('create-task')
+if(createTaskContainer) {
+  ReactDOM.createRoot(createTaskContainer!).render(createElement(CreateTaskApp))
+}
