b3e0ab9899a8fdda54bf3886a9263178f9298218
khikmatjon <azizovxikmatjon04@gmail.com>
2024-08-22 15:57:04 +0500

    fixed type

--- shared/contract/event-bus.ts
+++ shared/contract/event-bus.ts
@@ -11,7 +11,14 @@ export type EventBusEvents = keyof EventBusPayloads
 
 export type EventBusAction<T = any> = (props: T) => void;
 
+// only use for typing
 export class EventBus {  
-  on<K extends EventBusEvents>(event: K, callback: EventBusAction<EventBusPayloads[K]>) {}
-  emit<K extends EventBusEvents>(event: K, props: EventBusPayloads[K]) {}
+  on<K extends EventBusEvents>(event: K, callback: EventBusAction<EventBusPayloads[K]>) {
+    event;
+    callback;
+  }
+  emit<K extends EventBusEvents>(event: K, props: EventBusPayloads[K]) {
+    event;
+    props;
+  }
 }
