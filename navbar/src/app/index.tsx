c038d5c4b52da59de7b4aeb303c0a7a6453aff38
khikmatjon <azizovxikmatjon04@gmail.com>
2024-09-02 11:42:29 +0500

    fixed mini bugs

--- navbar/src/app/index.tsx
+++ navbar/src/app/index.tsx
@@ -1,24 +1,23 @@
 import React, { useState } from "react";
 import s from './app.module.css';
 import { useUnit } from 'effector-react'
+import { Link, RouterProvider } from "atomic-router-react";
 
 // @ts-expect-error no type
-import { routes } from 'utils'
+import { routes, router } from 'utils'
 
 export const App: React.FC = () => {
   const [homeIsOpen] = useUnit([routes.home.$isOpened])
-  const handleClick = (link: 'home') => (event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
-    event.preventDefault();
-    switch(link) {
-      case 'home': routes.home.open()
-    }
-  }
   return (
-    <header className={s['header']}>
-      <h1 className={s['header__logo']}>Todo</h1>
-      <nav className={s['header__nav']}>
-        <a className={s['header__nav-link']} data-active={homeIsOpen} href="/" onClick={handleClick('home')}>Home</a>
-      </nav>
-    </header>
+    <RouterProvider router={router}>
+      <header className={s['header']}>
+        <h1 className={s['header__logo']}>Todo</h1>
+        <nav className={s['header__nav']}>
+          <Link className={s['header__nav-link']} data-active={homeIsOpen} to={routes.home}>
+            Home
+          </Link>
+        </nav>
+      </header>
+    </RouterProvider>
   )
 }
